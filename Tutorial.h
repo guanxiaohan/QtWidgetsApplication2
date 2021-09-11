02249ac57758c2f816e1f31c05e21f63777dc3f7
孝翰 管 <13792898752@163.com>
2021-09-11 09:54:04 +0800

    更改头文件

--- Tutorial.h
+++ Tutorial.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "ui_Tutorial.h"
+#include "textPage.h"
 #include <qmainwindow.h>
+#include <QtCore/QList>
 
 namespace Ui {
 	class Tutorial;
@@ -9,4 +11,21 @@ namespace Ui {
 
 class Tutorial :public QMainWindow {
 	Q_OBJECT
+
+public:
+	Tutorial();
+	~Tutorial();
+	struct Tab {
+		QString file;
+		QString text;
+		textPage* widget;
+	};
+	QList<Tab*> Tabs;
+
+private:
+	Ui::Tutorial* ui;
+	textPage* currentTab;
+
+private slots:
+
 };
