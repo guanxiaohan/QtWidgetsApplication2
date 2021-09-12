b42da67451d151013dcf91cf7f1154723923dbdd
孝翰 管 <13792898752@163.com>
2021-09-12 13:58:23 +0800

    fd

--- textPage.h
+++ textPage.h
@@ -30,10 +30,38 @@ public:
 	QString GetFile();
 	bool fileSaved = true;
 	Ui::textPage* ui;
-	void colorText();
 	QTimer* timer = new QTimer();
 	
 private:
 	QString currentFile;
 	void unSave() { fileSaved = false; };
+};
+
+class IntelliSence:public QSyntaxHighlighter
+{
+    Q_OBJECT
+
+public:
+    IntelliSence(QTextDocument * parent = 0);
+
+protected:
+    void IntelliSenceBlock(const QString& text) override;
+
+private:
+    struct HighlightingRule
+    {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+    };
+    QVector<HighlightingRule> highlightingRules;
+
+    QRegularExpression commentStartExpression;
+    QRegularExpression commentEndExpression;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat classFormat;
+    QTextCharFormat singleLineCommentFormat;
+    QTextCharFormat multiLineCommentFormat;
+    QTextCharFormat quotationFormat;
+    QTextCharFormat functionFormat;
 };
