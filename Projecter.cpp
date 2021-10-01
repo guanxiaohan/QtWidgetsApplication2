39c148c7e2c7e8cd5a5b0fb7b6a84257e88b8b4f
孝翰 管 <13792898752@163.com>
2021-10-01 20:19:33 +0800

    fasdfas

--- Projecter.cpp
+++ Projecter.cpp
@@ -1,1 +1,112 @@
 #include "Projecter.h"
+
+Projecter::Projecter(QString fileUrl)
+{
+	QFile file(fileUrl);
+	QTextStream out(&file);
+	if (QFileInfo(fileUrl).suffix().toLower() == QString(".iep")) {
+		if (file.open(QFile::ReadOnly)) {
+			QString ProjectText = out.readAll();
+			file.close();
+			ProjectText.replace("\n", "");
+			ProjectText.replace("\t", "");
+			ProjectText.replace(" ", "");
+			auto Arguments = ProjectText.split(";;");
+			ProjectName = Arguments[0];
+			ProjectType = Arguments[1];
+			auto Files = Arguments[2].split(";");
+			for (auto file : Files) {
+				auto FileArguments = file.split("//");
+				File m_file;
+				m_file.FileName = FileArguments[0];
+				switch (FileArguments[1].toInt()) {
+				case 0:m_file.FileType = Unknown;
+				case 1:m_file.FileType = Unset;
+				case 2:m_file.FileType = Text;
+				case 3:m_file.FileType = Cpp;
+				case 4:m_file.FileType = Cpp_Header;
+				case 5:m_file.FileType = C;
+				case 6:m_file.FileType = Dll;
+				case 7:m_file.FileType = Python;
+				case 8:m_file.FileType = Json;
+				case 9:m_file.FileType = HTML;
+				case 10:m_file.FileType = Xml;
+				case 11:m_file.FileType = CSS;
+				case 12:m_file.FileType = JavaScript;
+				case 13:m_file.FileType = SQL;
+				case 14:m_file.FileType = VB;
+				case 15:m_file.FileType = VBS;
+				case 16:m_file.FileType = Java;
+				//Not all, add it later.
+				}
+				m_file.fileId = FileArguments[2].toInt();
+
+			}
+		}
+		else {
+			abort();
+		}
+	}
+	else {
+		qDebug() << "Not .iep file";
+	}
+}
+
+Projecter::~Projecter()
+{
+}
+
+Projecter::File Projecter::addFile(QString fileName, fileTypes fileType = Unset, int fileId = 1)
+{
+	return File();
+}
+
+Projecter::File Projecter::removeFile(QString fileName)
+{
+	return File();
+}
+
+Projecter::File Projecter::removeFile(int fileId)
+{
+	return File();
+}
+
+QList<Projecter::File> Projecter::removeFiles(fileTypes type)
+{
+	return QList<File>();
+}
+
+QList<Projecter::File> Projecter::removeFiles(QList<fileTypes> types)
+{
+	return QList<File>();
+}
+
+bool Projecter::setFile(int fileId, QString fileName)
+{
+	return false;
+}
+
+Projecter::File Projecter::fileAt(int fileId)
+{
+	return File();
+}
+
+QList<Projecter::File> Projecter::filesOfType(fileTypes type)
+{
+	return QList<File>();
+}
+
+int Projecter::idOf(File file)
+{
+	return 0;
+}
+
+int Projecter::idOf(QString fileName, fileTypes type)
+{
+	return 0;
+}
+
+QList<Projecter::File> Projecter::allFiles()
+{
+	return QList<File>();
+}
--- Projecter.h
+++ Projecter.h
@@ -7,9 +7,37 @@
 
 class Projecter:public QObject
 {
-private:
+public:
+	enum fileTypes {
+		Unknown,
+		Unset, Text, Cpp, Cpp_Header, C, Dll, Lib, Python, Json, HTML, Xml, CSS, JavaScript, SQL, VB, VBS, Java,
+		Unset_picture, Png, Jpg, Svg, Bmp, Ico,
+		Unset_audio, Mp3, Wav, Ogg, Flav
+	};
 	QString ProjectName;
 	QString ProjectType;
-	QList<QString> ProjectFiles;
+	struct File {
+		QString FileName;
+		fileTypes FileType;
+		int fileId;
+	};
+	QList<File> ProjectFiles;
+
+	Projecter(QString fileUrl);
+	~Projecter();
+
+	File addFile(QString fileName, fileTypes fileType = Unset, int fileId = 1);
+	File removeFile(QString fileName);
+	File removeFile(int fileId);
+	QList<File> removeFiles(fileTypes type);
+	QList<File> removeFiles(QList<fileTypes> types);
+	bool setFile(int fileId, QString fileName);
+
+	File fileAt(int fileId);
+	QList<File> filesOfType(fileTypes type);
+	int idOf(File file);
+	int idOf(QString fileName, fileTypes type = Unknown);
+
+	QList<File> allFiles();
 };
 
